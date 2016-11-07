5f7042028469560469a57b3d6dd982d854fcbc9e
lucky-hm <673619783@qq.com>
2016-11-07 18:05:34 +0800

    控制器优化，新增高清图

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,7 @@
 angular.module('app.controllers', [])
-
+  .config(function($ionicConfigProvider) {
+    // $ionicConfigProvider.views.transition('none');
+  })
   .controller('loginCtrl', function ($scope, $state, $ionicViewSwitcher) {
     $scope.login = true;
     $scope.signSelect = function () {
@@ -11,12 +13,18 @@ angular.module('app.controllers', [])
       // $ionicViewSwitcher.nextDirection("forward");
     }
   })
-  .controller('mainCtrl', function ($scope, $state, $http, $q, $ionicViewSwitcher) {
+  .controller('mainCtrl', function ($scope, $state, $http, $q, $ionicViewSwitcher,model302) {
     $scope.name = '管理员';
-
     $scope.nowModule = 1;
+    //控制顶部和底部是否隐藏
     $scope.headShow = true;
     $scope.footerShow = true;
+    $scope.footerStatus = function (status) {
+      $scope.footerShow = status;
+    };
+    $scope.headStatus = function (status) {
+      $scope.headShow = status;
+    };
     $scope.moduleChange = function (module_id, room_id) {
       // if($scope.nowModule!=module_id){
       console.log(module_id + '--' + room_id);
@@ -66,59 +74,15 @@ angular.module('app.controllers', [])
       $scope.nowModule = module_id;
       // }
     };
-    $scope.footerStatus = function (status) {
-      $scope.footerShow = status;
-    };
-    $scope.headStatus = function (status) {
-      $scope.headShow = status;
-    };
     $scope.icon_on = 0;
     $scope.iconChange = function (icon_id) {
       $scope.icon_on = icon_id;
     };
 
     $scope.loginOut = function () {
-
       $scope.moduleChange(0, 0);
-      // $ionicViewSwitcher.nextDirection("back");
     };
 
-    $scope.footerStatus = function (status, $event) {
-      $scope.footerShow = status;
-    };
-    
-    $scope.sendHttpRequest = function (room_id, deviceId, value) {
-      var deferred = $q.defer();
-      $http({
-        url: 'http://120.24.45.231:3000/control',
-        method: "POST",
-        data: {
-          // room_id:'301',deviceId:'22ff33xxdd',value:'00ff'
-          room_id: room_id, deviceId: deviceId, value: value
-        },
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        transformRequest: function (obj) {
-          var str = [];
-          for (var p in obj) {
-            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-          }
-          return str.join("&");
-        }
-
-      }).success(function (data) {
-        if (data.code == 200) {
-          deferred.resolve(data);
-        }
-        else {
-          deferred.reject(data.message);
-        }
-      }).error(function (data) {
-        deferred.reject(data.message);
-      });
-      return deferred.promise;
-    };
   })
   .controller('homeCtrl', function ($scope, $state) {
     $scope.headShow = true;
@@ -137,245 +101,77 @@ angular.module('app.controllers', [])
   /**
    * 儿童房Controller
    */
-  .controller('room1Ctrl', function ($scope, $state) {
+  .controller('room1Ctrl', function ($scope,model302,Switch) {
     $scope.moduleChange(1, 1);
-
-
-    $scope.switchConfig = [
-      {index: 1, name: '窗帘', device_id: '0B141152', state: false},  //儿童房窗帘|小孩房窗帘|小孩房纱帘
-      {index: 2, name: '台灯', device_id: '022C0FB7', state: false},  //儿童房灯|小孩房灯|儿童房阳台灯|小孩房阳台灯
-      {index: 3, name: '主灯', device_id: '021C0FB7', state: false},  //儿童房灯|小孩房灯|儿童房主灯|小孩房主灯
-      {index: 4, name: '空调', device_id: '', state: false},
-      {index: 5, name: '电器', device_id: '', state: false}
-    ];
-    $scope.switchState = {
-      open: '00ff',
-      close: '0000'
-    };
-
-    $scope.clickHandle = function (item) {
-      if (item.device_id == "") {
-        alert('未获取到设备号!');
-        return;
-      }
-      item.state = !item.state;
-      var room_id = "301";
-      var device_id = "";
-      var value = "";
-      device_id = item.device_id;
-      if (item.state) {
-        value = $scope.switchState.close;
-      }
-      else {
-        value = $scope.switchState.open;
-      }
-      $scope.sendHttpRequest(room_id, device_id, value).then(function (data) {
-        if (data.code == 200) {
-
-        }
-        else {
-          item.state = !item.state;
-          alert(data.data.content);
-        }
-      })
-    };
-
-
+    $scope.nowDevice = model302.childrenRoom;
+    $scope.changeStatus = function(name){
+      Switch.$set(model302.room_id,$scope.nowDevice[name]).then(function(data){
+        $scope.nowDevice[name][2] == '0000' ? $scope.nowDevice[name][2] = '00ff':$scope.nowDevice[name][2] = '0000';
+      });
+    }
   })
   /**
    * 次卧Controller
    */
-  .controller('room2Ctrl', function ($scope, $state) {
+  .controller('room2Ctrl', function ($scope, $state,model302,Switch) {
     $scope.moduleChange(1, 2);
-
+    $scope.nowDevice = model302.subalternRoom;
+    $scope.changeStatus = function(name){
+      Switch.$set(model302.room_id,$scope.nowDevice[name]).then(function(data){
+        $scope.nowDevice[name][2] == '0000' ? $scope.nowDevice[name][2] = '00ff':$scope.nowDevice[name][2] = '0000';
+      });
+    }
   })
   /**
    * 主卧Controller
    */
-  .controller('room3Ctrl', function ($scope, $state) {
+  .controller('room3Ctrl', function ($scope, $state,model302,Switch) {
     $scope.moduleChange(1, 3);
-
-
-
-    $scope.switchConfig = [
-      {index: 1, name: '风扇', device_id: '', state: false},
-      {index: 2, name: '射灯', device_id: '023C0FFF', state: false},
-      {index: 3, name: '窗帘', device_id: '', state: false},
-      {index: 4, name: '台灯', device_id: '024C0FFF', state: false},
-      {index: 5, name: '主灯', device_id: '021C0FFF', state: false},
-      {index: 6, name: '灯带', device_id: '024C0FFF', state: false},
-      {index: 7, name: '窗帘', device_id: '0B241161', state: false},
-      {index: 8, name: '空调', device_id: '', state: false},
-      {index: 9, name: '电器', device_id: '', state: false}
-    ];
-    $scope.switchState = {
-      open: '00ff',
-      close: '0000'
-    };
-
-    $scope.clickHandle = function (item) {
-      if (item.device_id == "") {
-        alert('未获取到设备号!');
-        return;
-      }
-      item.state = !item.state;
-      var room_id = "301";
-      var device_id = "";
-      var value = "";
-      device_id = item.device_id;
-      if (item.state) {
-        value = $scope.switchState.close;
-      }
-      else {
-        value = $scope.switchState.open;
-      }
-      $scope.sendHttpRequest(room_id, device_id, value).then(function (data) {
-        if (data.code == 200) {
-
-        }
-        else {
-          item.state = !item.state;
-          alert(data.data.content);
-        }
-      })
-    };
+    $scope.nowDevice = model302.masterRoom;
+    $scope.changeStatus = function(name){
+      Switch.$set(model302.room_id,$scope.nowDevice[name]).then(function(data){
+        $scope.nowDevice[name][2] == '0000' ? $scope.nowDevice[name][2] = '00ff':$scope.nowDevice[name][2] = '0000';
+      });
+    }
   })
 
   /**
    * 厨房Controller
    */
-  .controller('room4Ctrl', function ($scope, $state) {
+  .controller('room4Ctrl', function ($scope, $state,model302,Switch) {
     $scope.moduleChange(1, 4);
-
-
-    $scope.switchConfig = [
-      {index: 1, name: '厨房灯', device_id: '02105153', state: false}
-    ];
-    $scope.switchState = {
-      open: '00ff',
-      close: '0000'
-    };
-
-    $scope.clickHandle = function (item) {
-      if (item.device_id == "") {
-        alert('未获取到设备号!');
-        return;
-      }
-      item.state = !item.state;
-      var room_id = "301";
-      var device_id = "";
-      var value = "";
-      device_id = item.device_id;
-      if (item.state) {
-        value = $scope.switchState.close;
-      }
-      else {
-        value = $scope.switchState.open;
-      }
-      $scope.sendHttpRequest(room_id, device_id, value).then(function (data) {
-        if (data.code == 200) {
-
-        }
-        else {
-          item.state = !item.state;
-          alert(data.data.content);
-        }
-      })
-    };
-
-
+    $scope.nowDevice = model302.kitchenRoom;
+    $scope.changeStatus = function(name){
+      Switch.$set(model302.room_id,$scope.nowDevice[name]).then(function(data){
+        $scope.nowDevice[name][2] == '0000' ? $scope.nowDevice[name][2] = '00ff':$scope.nowDevice[name][2] = '0000';
+      });
+    }
   })
   /**
    * 餐厅Controller
    */
-  .controller('room5Ctrl', function ($scope, $state) {
+  .controller('room5Ctrl', function ($scope, $state,model302,Switch) {
     $scope.moduleChange(1, 5);
-
-    $scope.switchConfig = [
-      {index: 1, name: '主灯', device_id: '021C1070', state: false},
-      {index: 2, name: '射灯', device_id: '022C153D', state: false},
-      {index: 3, name: '灯带', device_id: '023C153D', state: false}
-    ];
-    $scope.switchState = {
-      open: '00ff',
-      close: '0000'
-    };
-
-    $scope.clickHandle = function (item) {
-      if (item.device_id == "") {
-        alert('未获取到设备号!');
-        return;
-      }
-      item.state = !item.state;
-      var room_id = "301";
-      var device_id = "";
-      var value = "";
-      device_id = item.device_id;
-      if (item.state) {
-        value = $scope.switchState.close;
-      }
-      else {
-        value = $scope.switchState.open;
-      }
-      $scope.sendHttpRequest(room_id, device_id, value).then(function (data) {
-        if (data.code == 200) {
-
-        }
-        else {
-          item.state = !item.state;
-          alert(data.data.content);
-        }
-      })
-    };
+    $scope.nowDevice = model302.diningRoom;
+    $scope.changeStatus = function(name){
+      Switch.$set(model302.room_id,$scope.nowDevice[name]).then(function(data){
+        $scope.nowDevice[name][2] == '0000' ? $scope.nowDevice[name][2] = '00ff':$scope.nowDevice[name][2] = '0000';
+      });
+    }
 
   })
   /**
    * 客厅Controller
    */
-  .controller('room6Ctrl', function ($scope, $state) {
+  .controller('room6Ctrl', function ($scope, $state,model302,Switch) {
     $scope.moduleChange(1, 6);
-
-    $scope.switchConfig = [
-      {index: 1, name: '主灯', device_id: '021C0FF7', state: false},
-      {index: 2, name: '壁灯', device_id: '022C1079', state: false},
-      {index: 3, name: '灯带', device_id: '023C1079', state: false},
-      {index: 4, name: '射灯', device_id: '024C1079', state: false},
-      {index: 5, name: '阳台灯', device_id: '02105169', state: false}
-    ];
-    $scope.switchState = {
-      open: '00ff',
-      close: '0000'
-    };
-
-    $scope.clickHandle = function (item) {
-      if (item.device_id == "") {
-        alert('未获取到设备号!');
-        return;
-      }
-      item.state = !item.state;
-      var room_id = "301";
-      var device_id = "";
-      var value = "";
-      device_id = item.device_id;
-      if (item.state) {
-        value = $scope.switchState.close;
-      }
-      else {
-        value = $scope.switchState.open;
-      }
-      $scope.sendHttpRequest(room_id, device_id, value).then(function (data) {
-        if (data.code == 200) {
-
-        }
-        else {
-          item.state = !item.state;
-          alert(data.data.content);
-        }
-      })
-    };
-
+    $scope.nowDevice = model302.livingRoom;
+    $scope.changeStatus = function(name){
+      Switch.$set(model302.room_id,$scope.nowDevice[name]).then(function(data){
+        $scope.nowDevice[name][2] == '0000' ? $scope.nowDevice[name][2] = '00ff':$scope.nowDevice[name][2] = '0000';
+      });
+    }
   })
-
 
   .controller('societyCtrl', function ($scope, $state) {
     $scope.moduleChange(2, 0);
--- js/services.js
+++ js/services.js
@@ -1,50 +1,124 @@
 angular.module('app.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
-
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
-    }
+.value('model302',{
+  'room_id':'302',
+  //['显示名称','设备ID','开关状态(0000=关闭,00ff=开启)','样式类'],同一房间中键名不要重复
+  //小孩房
+  'childrenRoom':{
+    'kingLight':['主灯','021C1664','0000','lamp'],
+    'balconyLamp':['阳台灯','022C1664','0000','lamp'],
+    'LampWith':['灯带','023C1664','0000','lamp'],
+    'drape1':['布帘','0B1411A0','0000','curtain'],
+    'drape2':['纱帘','0B2411A0','0000','curtain'],
+    'appliances':['电器','7600016A','0000','appliances']
+  },
+  //次卧、老人房
+  'subalternRoom':{
+    'kingLight':['主灯','02182BCA','0000','lamp'],
+    'balconyLamp':['阳台灯','02282BCA','0000','lamp'],
+    'LampWith':['灯带','02382BCA','0000','lamp'],
+    'doorLamp':['门口灯','021050C6','0000','lamp'],
+    'drape1':['布帘','0B141178','0000','curtain'],
+    'drape2':['纱帘','0B241178','0000','curtain'],
+    'appliances':['电器','760001D8','0000','appliances']
+  },
+  //主卧
+  'masterRoom':{
+    // 'kingLight':['主灯','021C1678','0000','lamp'],
+    'bedLamp':['床头灯','023C1678','0000','lamp'],
+    'shootLamp':['射灯','02382C0B','0000','lamp'],
+    'LampWith':['灯带','024C1678','0000','lamp'],
+    'drape1':['布帘','0B14115F','0000','curtain'],
+    'drape2':['纱帘','0B24115F','0000','curtain'],
+    'appliances':['电器','76000162','0000','appliances'],
+    'pushWindow':['推窗器','0B100BBC','0000','appliances']
+  },
+  //客厅
+  'livingRoom':{
+    'kingLight':['主灯','021C1607','0000','lamp'],
+    'balconyLamp':['阳台灯','021051B5','0000','lamp'],
+    'LampWith':['灯带','022C1607','0000','lamp'],
+    'shootLamp':['射灯','023C1607','0000','lamp'],
+    'wallLamp':['壁灯','024C1607','0000','lamp'],
+    'drape1':['布帘','0B1411E4','0000','curtain'],
+    'drape2':['纱帘','0B2411E4','0000','curtain'],
+    'appliances':['电器','7600015D','0000','appliances'],
+    'socket1':['电视机插座','13104853','0000','appliances'],
+    'socket2':['插座2','13204853','0000','appliances'],
+    'socket3':['插座3','13304853','0000','appliances'],
+    'socket4':['机顶盒插座','13404853','0000','appliances'],
+  },
+  //餐厅
+  'diningRoom':{
+    'kingLight':['主灯','021C159E','0000','lamp'],
+    'shootLamp':['射灯','022C159E','0000','lamp'],
+    'LampWith':['灯带','024C159E','0000','lamp'],
+    'LampWith':['鞋柜灯','023C159E','0000','lamp'],
+  },
+  //厨房
+  'kitchenRoom':{
+    'kingLight':['主灯','02105153','0000','lamp'],
+    'balconyLamp':['阳台灯','02105111','0000','lamp'],
+    'socket':['水阀插座','031802E4','0000','appliances'],
+  },
+  //主卫
+  'masterRestRoom':{
+    'kingLight':['主灯','02182BC4','0000','lamp'],
+    'bathroomLamp':['浴室灯','02282BC4','0000','lamp'],
+    'LensHeadLight':['镜前灯','02382BC4','0000','lamp'],
+  },
+  //公卫
+  'restRoom':{
+    'kingLight':['主灯','02182BBE','0000','lamp'],
+    'LensHeadLight':['镜前灯','02282BBE','0000','lamp'],
+    'exhaustAir':['排风','02382BBE','0000','fans'],
+  },
+  'leaveHome':['出门','7324002C','00000',''],
+  'goHome':['回家','7314002C','0000','']
+})
+.factory('Switch',function($http,$q){
+  //获取当前room的设备状态
+  var service = {};
+  service.$get=function(room_id){
+    return '……';
   };
-});
+  //设置某个设备的状态
+  service.$set = function (room_id, nowDevice) {
+    var deviceId = nowDevice[1];
+    var value = nowDevice[2];
+    value == '0000'?value='00ff':value='0000';
+    var deferred = $q.defer();
+    $http({
+      url: 'http://120.24.45.231:3000/control',
+      method: "POST",
+      data: {
+        room_id: room_id, deviceId: deviceId, value: value
+      },
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      transformRequest: function (obj) {
+        var str = [];
+        for (var p in obj) {
+          str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+        }
+        return str.join("&");
+      }
+
+    }).success(function (data) {
+      if (data.code == 200) {
+        deferred.resolve(data);
+      }
+      else {
+        deferred.reject(data.message);
+      }
+    }).error(function (data) {
+      deferred.reject(data.message);
+    });
+    return deferred.promise;
+
+  }
+ 
+  return service;
+
+})
